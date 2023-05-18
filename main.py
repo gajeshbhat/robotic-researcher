b94cad2792e7934ac29f0f83816e49da033af1a1
Gajesh Bhat <16153045+gajeshbhat@users.noreply.github.com>
2023-05-18 16:13:01 +0530

    Refactored Robot Class and added CLI features

--- main.py
+++ main.py
@@ -1,19 +1,55 @@
-from robotics import Robot
-from bs4 import BeautifulSoup
+import argparse
+import time
 import json
+from robotics import Robot, DEFAULT_SCIENTISTS
 
-def main() -> None:
-    # Initialize the robot and Introduce itself
-    scientist_robo = Robot("Quandrinaut",scientists=['Ada Lovelace', 'Grace Hopper', 'Lynn Conway'],summary_prediction=True,summary_prediction_model="en_core_web_lg")
+
+def save_to_json(data, output_path):
+    """
+    Save the data to a JSON file.
+    """
+    with open(output_path, 'w') as f:
+        json.dump(data, f, indent=4)
+
+
+def parse_args():
+    """
+    Parse command line arguments.
+    """
+    parser = argparse.ArgumentParser(description="Web scraper for scientists' data from Wikipedia.")
+    parser.add_argument('-s', '--scientists', nargs='+', default=DEFAULT_SCIENTISTS,
+                        help='Names of the scientists to scrape data for.')
+    parser.add_argument('-m', '--model', default='en_core_web_sm', help='Spacy model for summary prediction.')
+    parser.add_argument('-o', '--output', default='scientist_data.json', help='Output file for the scraped data.')
+    return parser.parse_args()
+
+
+def main():
+    """
+    Main function to execute the script.
+    """
+    args = parse_args()
+
+    # Start Measuring Robo's time
+    start_time = time.time()
+
+    # Initialize the robot and introduce itself
+    scientist_robo = Robot("Quandrinaut", scientists=args.scientists, summary_prediction=False, summary_prediction_model=args.model)
     scientist_robo.say_hello()
 
     scientist_data = scientist_robo.get_scientists_summary()
 
-    with open('data/scientist_data.json', 'w') as f:
-        json.dump(scientist_data, f, indent=4)
-
     # Say goodbye and close the browser
     scientist_robo.say_goodbye()
+    
+    # End Measuring Robo's time
+    end_time = time.time()
+
+    # Print the time taken by Robo and other details
+    print(f"Robo took {end_time - start_time} seconds to scrape data for {len(scientist_data)} scientist(s).")
+
+    # Save the data to a JSON file
+    save_to_json(scientist_data, args.output)
 
 if __name__ == "__main__":
     main()
--- robotics.py
+++ robotics.py
@@ -2,12 +2,11 @@ from RPA.Browser.Selenium import Selenium
 from bs4 import BeautifulSoup
 from datetime import datetime
 from typing import Optional, List
-from pathlib import Path
 import re
 import spacy
 import os
 
-DEFAULT_SCIENTISTS = ["Albert Einstein", "Isaac Newton", "Marie Curie", "Charles Darwin"]
+DEFAULT_SCIENTISTS = ["Albert Einstein", "Isaac Newton", "Marie Curie", "Charles Darwin",'Ada Lovelace', 'Grace Hopper', 'Lynn Conway']
 BASE_URL = "https://en.wikipedia.org/wiki/"
 
 class Robot:
@@ -30,174 +29,187 @@ class Robot:
             self.scientists = scientists
 
         if self.summary_prediction:
-            # Define the model used for NER
             self.model_name = self.summary_prediction_model
-
-            if not os.path.exists(os.getcwd() + "/models/" + self.model_name + "/" ):
-                self.download_and_store_spacy_model()
-
-            # Load the model
-            self.nlp = spacy.load(os.getcwd() + "/models/" + self.model_name + "/")
+            self.ensure_spacy_model_exists()
+            self.nlp = spacy.load(self.get_model_path())
         
-    
+    def ensure_spacy_model_exists(self):
+        """
+        Check if the model exists, if not download and store it.
+        """
+        if not os.path.exists(self.get_model_path()):
+            self.download_and_store_spacy_model()
+
+    def get_model_path(self):
+        """
+        Returns the path of the spacy model.
+        """
+        return os.path.join(os.getcwd(), "models", self.model_name)
+
     def download_and_store_spacy_model(self):
-        if not os.path.exists(os.getcwd() + "/models/"+self.model_name):
-            os.makedirs(os.getcwd() + "/models/"+ self.model_name, exist_ok=True)
-
-        # Download the model    
+        """
+        Downloads and stores the spacy model.
+        """
+        if not os.path.exists(os.path.join(os.getcwd(), "models")):
+            os.makedirs(os.path.join(os.getcwd(), "models/{self.model_name}}/"), exist_ok=True)
         spacy.cli.download(self.model_name)
-        
-        # Save the model
         nlp = spacy.load(self.model_name)
-        nlp.to_disk(os.getcwd() + "/models/"+self.model_name+"/")
-        
-        
-            
-    def infer_field_of_work(self, intro: str) -> str:
-        # Load the small English model
-        field_of_work_entities = []
-        doc = self.nlp(intro)
+        nlp.to_disk(self.get_model_path())
 
-        for ent in doc.ents:
-            if ent.label_ in ["ORG", "PRODUCT", "WORK_OF_ART", "EVENT", "LAW", "LANGUAGE", "NORP"]:
-                field_of_work_entities.append(ent.text)
+    def get_scientist_page_soup(self, scientist: str) -> BeautifulSoup:
+        """
+        Fetches the scientist page and returns the parsed BeautifulSoup object.
+        """
+        scientist_info_url = BASE_URL + scientist.replace(' ', '_')
+        self._open_webpage(scientist_info_url)
+        return BeautifulSoup(self._get_page_source(), "html.parser")
 
-        # Remove duplicates and join the keywords
-        field_of_work = list(set(field_of_work_entities))
-    
-        if field_of_work:
-            return ', '.join(field_of_work)
-        else:
-            return 'Not specified'
+    def _open_webpage(self, webpage: str):
+        """
+        Opens the webpage.
+        """
+        try:
+            self.browser.go_to(webpage)
+        except Exception as e:
+            print(f"Error while opening webpage: {e}")
 
+    def _get_page_source(self) -> str:
+        """
+        Returns the page source of the current webpage.
+        """
+        try:
+            return self.browser.get_source()
+        except Exception as e:
+            print(f"Error while getting page source: {e}")
+            return ""
+
+    def _close_browser(self):
+        """
+        Closes the browser.
+        """
+        try:
+            self.browser.close_all_browsers()
+        except Exception as e:
+            print(f"Error while closing browser: {e}")
+
+    def clean_text(self, text: str) -> str:
+        """
+        Cleans the text by removing reference tags, extra whitespaces and newline characters.
+        """
+        text = re.sub(r'\[\d+\]', '', text)  # Remove reference tags
+        text = re.sub(r'\s+', ' ', text)  # Remove extra whitespace and newline characters
+        return text.strip()
 
     def get_scientists_summary(self) -> List[dict]:
         """
         Returns a list of dictionaries containing the information of the scientists.
         """
-        scientist_data = []
-        for scientist in self.scientists:
-            scientist_details = self.parse_scientist_data(scientist)
-            scientist_data.append(scientist_details)
-        return scientist_data
-    
+        return [self.parse_scientist_data(scientist) for scientist in self.scientists]
+
     def parse_scientist_data(self, scientist: str) -> dict:
-        soup = self.get_scientist_page_soup(scientist)
-        born, died = self.find_birth_death_dates(soup)
-        age = self.calculate_age(born, died)
-        
-        born = born.replace('(', '').replace(')', '')
-        died = died.replace('(', '').replace(')', '')
-        
-        intro = self.get_first_paragraph(soup)
-        field_of_work = self.get_field_of_work(soup)
-        
-        if field_of_work == 'Not specified' and intro != 'unknown' and self.summary_prediction:
-            field_of_work = self.infer_field_of_work(intro) + " (Predicted)"
-        
-        birthplace = self.get_birthplace(soup)
-        
-        self.display_info(scientist, born, died, age, intro, field_of_work, birthplace)
-        
-        return {
-        'name': scientist,
-        'born': born,
-        'died': died,
-        'age': age,
-        'introduction': intro,
-        'field_of_work': field_of_work,
-        'birthplace': birthplace
-        }
+        """
+        Returns a dictionary containing the information of the scientist.
+        """
+        try:
+            soup = self.get_scientist_page_soup(scientist)
+            born, died = self.find_birth_death_dates(soup)
+            age = self.calculate_age(born, died)
 
-    def say_hello(self):
-        "Introduces itself to the user and describes what it does"
-        print(f"Hello, my name is {self.name}")
+            born = born.replace('(', '').replace(')', '')
+            died = died.replace('(', '').replace(')', '')
 
-    def _open_webpage(self, webpage: str):
-        self.browser.go_to(webpage)
+            intro = self.get_first_paragraph(soup)
+            field_of_work = self.get_field_of_work(soup)
 
-    def get_scientist_page_soup(self, scientist: str) -> str:
-        scientist_info_url = BASE_URL + scientist.replace(' ', '_')
-        self._open_webpage(scientist_info_url)
-        soup = BeautifulSoup(self._get_page_source(), "html.parser")
-        return soup
+            if field_of_work == 'Not specified' and intro != 'unknown' and self.summary_prediction:
+                field_of_work = self.infer_field_of_work(intro) + " (Predicted)"
+
+            birthplace = self.get_birthplace(soup)
+
+            self.display_info(scientist, born, died, age, intro, field_of_work, birthplace)
+
+            return {
+                'name': scientist,
+                'born': born,
+                'died': died,
+                'age': age,
+                'introduction': intro,
+                'field_of_work': field_of_work,
+                'birthplace': birthplace
+            }
+        except Exception as e:
+            print(f"Error while parsing scientist data: {e}")
+            return {}
     
-    def _get_page_source(self) -> str:
-        return self.browser.get_source()
+    def infer_field_of_work(self, intro: str) -> str:
+        """
+        Infers the field of work from the introduction.
+        """
+        doc = self.nlp(intro)
+        field_of_work_entities = [ent.text for ent in doc.ents if ent.label_ in ["ORG", "PRODUCT", "WORK_OF_ART", "EVENT", "LAW", "LANGUAGE", "NORP"]]
+        field_of_work = ', '.join(list(set(field_of_work_entities)))  # Remove duplicates and join the keywords
 
-    def say_goodbye(self):
-        print(f"Goodbye, my name is {self.name}")
-        self._close_browser()
-
-    # Private method to close the browser
-    def _close_browser(self):
-        self.browser.close_all_browsers()
+        return field_of_work if field_of_work else 'Not specified'
 
     def find_birth_death_dates(self, soup: BeautifulSoup) -> tuple:
-        born, died = 'unknown', 'unknown'
-
-        born_data = soup.find('th', string='Born')
-        if born_data:
-            born_date = born_data.find_next_sibling('td').find('span')
-            if born_date:
-                born = born_date.text.strip()
-
-        died_data = soup.find('th', string='Died')
-        if died_data:
-            died_date = died_data.find_next_sibling('td').find('span')
-            if died_date:
-                died = died_date.text.strip()
+        """
+        Finds the birth and death dates of the scientist from the soup object.
+        """
+        born = self.find_date(soup, 'Born')
+        died = self.find_date(soup, 'Died')
 
         if born != 'unknown' and died == 'unknown':
             died = 'Present'
-        
+
         return born, died
 
+    def find_date(self, soup: BeautifulSoup, date_type: str) -> str:
+        """
+        Finds a date (born or died) of the scientist from the soup object.
+        """
+        try:
+            date_data = soup.find('th', string=date_type)
+            date = date_data.find_next_sibling('td').find('span').text.strip() if date_data else 'unknown'
+        except Exception as e:
+            print(f"Error while finding {date_type} date: {e}")
+            date = 'unknown'
+        return date
+
     def calculate_age(self, born: str, died: str) -> str:
-        if born == 'unknown' and died == 'unknown':
-            return 'unknown'
-        
-        if died == 'Present':
+        """
+        Calculates the age of the scientist.
+        """
+        try:
             born_date = datetime.strptime(born, "(%Y-%m-%d)")
-            today = datetime.today()
-            age = (today - born_date).days // 365
-
-            return age
-
-        born_date = datetime.strptime(born, "(%Y-%m-%d)")
-        died_date = datetime.strptime(died, "(%Y-%m-%d)")
-        age = (died_date - born_date).days // 365
-
+            died_date = datetime.strptime(died, "(%Y-%m-%d)") if died != 'Present' else datetime.today()
+            age = (died_date - born_date).days // 365
+        except Exception as e:
+            print(f"Error while calculating age: {e}")
+            age = 'unknown'
         return age
-
+    
     def get_first_paragraph(self, soup: BeautifulSoup) -> str:
+        """
+        Gets the first paragraph of the scientist page.
+        """
         paragraphs = soup.find_all('p')
-
-        for paragraph in paragraphs:
-            text = paragraph.text.strip()
-
-            # Check if the paragraph has text
-            if len(text) > 0:
-                # Remove reference tags
-                text = re.sub(r'\[\d+\]', '', text)
-                # Remove extra whitespace and newline characters
-                text = re.sub(r'\s+', ' ', text)
-                return text
-
-        # If no paragraph with text is found
-        return 'Information not available.'
-
-    def display_info(self, scientist: str, born: str, died: str, age: str, intro: str,
-                    field_of_work: str, birthplace: str) -> None:
-        print(f"Name: {scientist}")
-        print(f"Born: {born}")
-        print(f"Died: {died}")
-        print(f"Age: {age}")
-        print(f"Birthplace: {birthplace}\n")
-        print(f"Introduction: {intro}\n")
-        print(f"Field of work: {field_of_work}\n")
+        try:
+            for paragraph in paragraphs:
+                text = paragraph.text.strip()
+                # Check if the paragraph has text
+                if len(text) > 0:
+                    text = self.clean_text(text)
+                    return text
+            # If no paragraph with text is found
+            return 'Information not available.'
+        except Exception as e:
+            print(f"Error while getting first paragraph: {e}")
+            return 'unknown'
 
     def get_field_of_work(self, soup: BeautifulSoup) -> str:
+        """
+            Gets the field of work of the scientist.
+        """
         infobox = soup.find('table', {'class': 'infobox'})
         if not infobox:
             return 'Not specified'
@@ -212,38 +224,67 @@ class Robot:
 
         fields = fields_td.find_all('a')
         field_of_work = ', '.join([field.text for field in fields])
-
-        # Remove reference tags
-        field_of_work = re.sub(r'\[\d+\]', '', field_of_work)
-
-        # Remove extra whitespace and newline characters
-        field_of_work = re.sub(r'\s+', ' ', field_of_work)
-
-        return field_of_work
+        return self.clean_text(field_of_work) if field_of_work else 'Not specified'
 
     def get_birthplace(self, soup: BeautifulSoup) -> str:
-        infobox = soup.find('table', {'class': 'infobox'})
-        if not infobox:
+        """
+        Gets the birthplace of the scientist.
+        """
+        try:
+            infobox = soup.find('table', {'class': 'infobox'})
+            if not infobox:
+                return 'Not specified'
+
+            birthplace_data = infobox.find('th', string='Born')
+            if not birthplace_data:
+                return 'Not specified'
+
+            birthplace_td = birthplace_data.find_next_sibling('td')
+            if not birthplace_td:
+                return 'Not specified'
+
+            birthplace = birthplace_td.find('div', {'class': 'birthplace'})
+            if not birthplace:
+                return 'Not specified'
+
+            birthplace_text = birthplace.text.strip()
+            return self.clean_text(birthplace_text) if birthplace_text else 'Not specified'
+        except Exception as e:
+            print(f"Error while getting birthplace: {e}")
             return 'unknown'
 
-        birthplace_data = infobox.find('th', string='Born')
-        if not birthplace_data:
-            return 'unknown'
+    def clean_text(self, text: str) -> str:
+        """
+        Cleans the text.
+        """
+        text = text.strip()  # Remove whitespace at the beginning and end
+        text = re.sub(r'\[\d+\]', '', text)  # Remove reference tags
+        text = re.sub(r'\s+', ' ', text)  # Remove extra whitespace and newline characters
+        return text
+    
+    # Write a method to say hello and explain to user what the robot does and steps involved
+    def say_hello(self):
+        """
+        Says hello to the user and explains what the robot does and the steps involved.
+        """
+        print(f"\nHello, I am {self.name}! I am a robot that can scrape data from Wikipedia for scientists and predict their field of work using their introduction if not available on wikipedia.")
+        print("Let's get started!\n")
 
-        birthplace_td = birthplace_data.find_next_sibling('td')
-        if not birthplace_td:
-            return 'unknown'
+    def say_goodbye(self):
+        """
+        Says goodbye to the user.
+        """
+        print(f"\nGoodbye! I hope you liked my work.")
 
-        birthplace = birthplace_td.find('div', {'class': 'birthplace'})
-        if not birthplace:
-            return 'Not specified'
-
-        birthplace_text = birthplace.text.strip()
-
-        # Remove reference tags
-        birthplace_text = re.sub(r'\[\d+\]', '', birthplace_text)
-
-        # Remove extra whitespace and newline characters
-        birthplace_text = re.sub(r'\s+', ' ', birthplace_text)
-
-        return birthplace_text
+    def display_info(self, name: str, born: str, died: str, age: str, intro: str, field_of_work: str, birthplace: str):
+        """
+        Displays the information of the scientist.
+        """
+        print(f"\n{name}")
+        print(f"Born: {born}")
+        print(f"Died: {died}")
+        print(f"Age: {age}\n")
+        print(f"Introduction: {intro}\n")
+        print(f"Field of work: {field_of_work}")
+        print(f"Birthplace: {birthplace}")
+        print("----------------------------------------\n")
